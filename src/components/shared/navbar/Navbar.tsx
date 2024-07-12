d5783fae7749e7ac7b81f4230bf45170fa3c69ac
jakariamasum <jmphy698@gmail.com>
2024-07-12 23:52:49 +0600

    cart length show along icon

--- src/components/shared/navbar/Navbar.tsx
+++ src/components/shared/navbar/Navbar.tsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useAppSelector } from "../../../redux/hooks";
+import { RootState } from "../../../redux/store";
 
 const Navbar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const cartItems = useAppSelector((state: RootState) => state.cart.items);
+  console.log(cartItems);
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
@@ -44,7 +48,7 @@ const Navbar = () => {
       className="lg:mr-6 flex items-center"
       onClick={closeDropdown}
     >
-      <Link to="/cart" className="flex items-center">
+      <Link to="/cart" className="flex items-center gap-1 ">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
@@ -53,7 +57,7 @@ const Navbar = () => {
         >
           <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
         </svg>
-        {/* <span className="text-[#22A1F0]">{cartItem?.length || 0}</span> */}
+        <span className="text-[#22A1F0] ">{cartItems?.length || 0}</span>
       </Link>
     </li>,
   ];
