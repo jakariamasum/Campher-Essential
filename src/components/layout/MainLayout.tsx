7d7e3ef8bd541d07c8eb790d0ffc1f11d591e870
jakariamasum <jmphy698@gmail.com>
2024-07-12 23:58:51 +0600

    add warning when cartItems exists

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,8 +1,28 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../shared/navbar/Navbar";
 import Footer from "../shared/footer/Footer";
+import { useAppSelector } from "../../redux/hooks";
+import { useEffect } from "react";
+import { RootState } from "../../redux/store";
 
 const MainLayout = () => {
+  const cartItems = useAppSelector((state: RootState) => state.cart.items);
+
+  useEffect(() => {
+    const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+      if (cartItems.length > 0) {
+        event.preventDefault();
+        event.returnValue = "";
+      }
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [cartItems]);
+
   return (
     <div>
       <Navbar />
